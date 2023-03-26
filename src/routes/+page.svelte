1028192c1a8edd7405e1046e265837aa53d279d6
Tibet Tornaci <oofdere@hey.com>
2023-03-26 01:40:21 -0700

    make progress bar be in sync

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { metadata, countup } from '$lib/GlobalStore';
+	import { secondsToMinutes, formatDuration } from 'date-fns';
 </script>
 
 {#if $metadata}
@@ -10,9 +11,19 @@
 			<h2>{$metadata.artist}</h2>
 			<h3>{$metadata.album} {$metadata.circle ? `/ ${$metadata.circle}` : ''}</h3>
 
-			{$countup}
-			<progress class="w-72" max={$metadata.duration} value={$countup} />
-			{$metadata.duration - $countup}
+			{#if $countup}
+				{Math.trunc($countup / 60) +
+					':' +
+					($countup % 60).toLocaleString('en-US', {
+						minimumIntegerDigits: 2
+					})}
+				<progress class="w-72" max={$metadata.duration} value={$countup} />
+				{Math.trunc(($metadata.duration - $countup) / 60) +
+					':' +
+					(($metadata.duration - $countup) % 60).toLocaleString('en-US', {
+						minimumIntegerDigits: 2
+					})}
+			{/if}
 		</div>
 	</div>
 {/if}
